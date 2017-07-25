a89bd8d0095c4de739eb3fdc40b6008ba15719e0
Vidushan Kohiladas <vidushan@Vidushans-MacBook-Air.local>
2017-07-24 17:57:14 -0700

    used react to add elements onto index.html

---
+++ src/MessageList.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class MessageList extends Component{
+  render(){
+    return(
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+    );
+  }
+}
+export default MessageList;
--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,15 @@
 // Load up the application styles
 require("../styles/application.scss");
 
+
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+ReactDOM.render(<Message />, document.getElementById('messages'));
+ReactDOM.render(<ChatBar />, document.getElementById('chatbar'));
+ReactDOM.render(<MessageList />, document.getElementById('navbar'));
